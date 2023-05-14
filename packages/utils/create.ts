664172044c32ec04150ee80839a07a2a54111533
yeweimao <yeweimao@zhuanzhuan.com>
2023-05-14 18:32:45 +0800

    feat: 完成bem规范编写

--- packages/utils/create.ts
+++ packages/utils/create.ts
@@ -58,7 +58,7 @@ function createBEM(prefixName: string) {
   };
 }
 
-function createNamespace(name: string) {
+export function createNamespace(name: string) {
   const prefixName = `z-${name}`;
   return createBEM(prefixName);
 }
