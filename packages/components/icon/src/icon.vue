c44cfd398f209ea8458125886e49569d8a9b23c4
yeweimao <yeweimao@zhuanzhuan.com>
2023-05-21 12:53:07 +0800

    add: 新增eslint相关配置

--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -4,19 +4,19 @@
   </i>
 </template>
 <script setup lang="ts">
-import { createNamespace } from "@yeweimao/utils/create";
-import { computed } from "vue";
-import { iconProps } from "./icon";
+import { createNamespace } from '@yeweimao/utils/create'
+import { computed } from 'vue'
+import { iconProps } from './icon'
 defineOptions({
-  name: "z-icon",
-});
-const bem = createNamespace("icon");
-const props = defineProps(iconProps);
+  name: 'ZIcon'
+})
+const bem = createNamespace('icon')
+const props = defineProps(iconProps)
 const style = computed(() => {
-  if (!props.color && !props.size) return {};
+  if (!props.color && !props.size) return {}
   return {
     ...(props.size ? { fontSize: `${props.size}px` } : {}),
-    ...(props.color ? { color: props.color } : {}),
-  };
-});
+    ...(props.color ? { color: props.color } : {})
+  }
+})
 </script>
